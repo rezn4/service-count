7d2c91c6621a9493d19e545e7c35dbeefe01685d
rezn4 <rezn.dm@gmail.com>
2019-07-03 22:03:52 +0300

    infinite service creation bug fixed

--- app/src/main/java/android/rezndm/servicecount/MainActivity.kt
+++ app/src/main/java/android/rezndm/servicecount/MainActivity.kt
@@ -11,7 +11,7 @@ import android.rezndm.servicecount.Const.Companion.INTENT_ACTION_COUNT
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-
+import android.app.ActivityManager
 
 class MainActivity : AppCompatActivity(), MainView {
     private val intentFilter: IntentFilter = IntentFilter()
@@ -34,14 +34,32 @@ class MainActivity : AppCompatActivity(), MainView {
         service = Intent(this, CountService::class.java)
 
         btn_start_service.setOnClickListener {
-            startService(service)
-            Toast.makeText(this, getString(R.string.toast_service_started), Toast.LENGTH_SHORT).show()
+            if (!isServiceRunning(CountService::class.java)){
+                startService(service)
+                Toast.makeText(this, getString(R.string.toast_service_started), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, getString(R.string.toast_service_already_running), Toast.LENGTH_SHORT).show()
+            }
         }
 
         btn_stop_service.setOnClickListener {
-            stopService(service)
-            Toast.makeText(this, getString(R.string.toast_service_stopped), Toast.LENGTH_SHORT).show()
+            if (isServiceRunning(CountService::class.java)){
+                stopService(service)
+                Toast.makeText(this, getString(R.string.toast_service_stopped), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, getString(R.string.toast_service_isnt_started), Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 
     override fun showDate(date: String) {
